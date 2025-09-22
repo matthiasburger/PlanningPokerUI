5ae8613cf991d48bc5c5cdc2adc64f26f5d7b2ba
Matthias Burger <matthias.burger@axpo.com>
2025-09-23 00:41:34 +0200

    fixed wrong url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  hubUrl: 'planning-poker-bggad0a7afhxenej.switzerlandnorth-01.azurewebsites.net/hub'
+  hubUrl: 'https://planning-poker-bggad0a7afhxenej.switzerlandnorth-01.azurewebsites.net/hub'
 };
