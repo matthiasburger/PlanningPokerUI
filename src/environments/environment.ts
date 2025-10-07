f31c1ec45b7a4792079bab4e71ec83cb46981125
Matthias Burger <matthias.burger@axpo.com>
2025-10-07 10:48:34 +0200

    fixed hubUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // hubUrl: 'https://planning-poker-bggad0a7afhxenej.switzerlandnorth-01.azurewebsites.net/hub'
-  hubUrl: 'http://localhost:5283/hub'
+  hubUrl: 'https://planning-poker-bggad0a7afhxenej.switzerlandnorth-01.azurewebsites.net/hub'
+  // hubUrl: 'http://localhost:5283/hub'
 };
